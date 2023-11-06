e06c3a3c3a71e4a551c011d2aeaf8c93d9b75a28
Davi Petris <davi.petris@oracle.com>
2023-11-06 04:06:57 -0300

    modal handler

--- libs/user/handlers.go
+++ libs/user/handlers.go
@@ -1,9 +1,8 @@
 package user
 
 import (
-	"log"
-
 	"github.com/bwmarrin/discordgo"
+	uuid "github.com/satori/go.uuid"
 )
 
 func CommandHandlers() map[string]func(s *discordgo.Session, i *discordgo.InteractionCreate) {
@@ -12,14 +11,86 @@ func CommandHandlers() map[string]func(s *discordgo.Session, i *discordgo.Intera
 	}
 }
 
+// func addCommandHandler(s *discordgo.Session, i *discordgo.InteractionCreate) {
+// 	responseData := discordgo.InteractionResponseData{
+// 		Content: "Add command invoked. Here's your response!",
+// 		Flags:   discordgo.MessageFlagsEphemeral,
+// 	}
+// 	log.Print("handler called")
+// 	response := discordgo.InteractionResponse{
+// 		Type: discordgo.InteractionResponseChannelMessageWithSource,
+// 		Data: &responseData,
+// 	}
+
+// 	s.InteractionRespond(i.Interaction, &response)
+// }
+
 func addCommandHandler(s *discordgo.Session, i *discordgo.InteractionCreate) {
+	uuid := uuid.NewV4()
 	responseData := discordgo.InteractionResponseData{
-		Content: "Add command invoked. Here's your response!",
-		Flags:   discordgo.MessageFlagsEphemeral,
+		CustomID: "group_buy_item_" + i.Interaction.Member.User.ID + "_" + uuid.String(),
+		// Flags:    discordgo.MessageFlagsEphemeral,
+		Title: "Group buy - Request new product",
+		Components: []discordgo.MessageComponent{
+			discordgo.ActionsRow{
+				Components: []discordgo.MessageComponent{
+					discordgo.TextInput{
+						CustomID:    "product-url",
+						Label:       "Link do produto",
+						Style:       discordgo.TextInputShort,
+						Placeholder: "https://www.amazon.com/Images-You-Should-Not-Masturbate/dp/0399536493",
+						Required:    true,
+					},
+				},
+			},
+			discordgo.ActionsRow{
+				Components: []discordgo.MessageComponent{
+					discordgo.TextInput{
+						CustomID:    "price",
+						Label:       "Preço (incluindo o frete, se tiver)",
+						Placeholder: "Preço total em dólar ($)",
+						Style:       discordgo.TextInputShort,
+						Required:    true,
+					},
+				},
+			},
+			discordgo.ActionsRow{
+				Components: []discordgo.MessageComponent{
+					discordgo.TextInput{
+						CustomID:    "weight",
+						Label:       "Peso total (não esqueça da unidade de medida)",
+						Placeholder: "0.5kg ou 1lb",
+						Style:       discordgo.TextInputShort,
+						Required:    false,
+					},
+				},
+			},
+			discordgo.ActionsRow{
+				Components: []discordgo.MessageComponent{
+					discordgo.TextInput{
+						CustomID:    "tax-rate",
+						Label:       "Quanto você está disposto a pagar de imposto?",
+						Placeholder: "Taxa em porcentagem (0% não vale). Ex: 20%",
+						Style:       discordgo.TextInputShort,
+						Required:    true,
+					},
+				},
+			},
+			discordgo.ActionsRow{
+				Components: []discordgo.MessageComponent{
+					discordgo.TextInput{
+						CustomID:    "buyer-location",
+						Label:       "Onde você mora?",
+						Placeholder: "Pode ser somente a sigla do estado",
+						Style:       discordgo.TextInputShort,
+						Required:    true,
+					},
+				},
+			},
+		},
 	}
-	log.Print("handler called")
 	response := discordgo.InteractionResponse{
-		Type: discordgo.InteractionResponseChannelMessageWithSource,
+		Type: discordgo.InteractionResponseModal,
 		Data: &responseData,
 	}
 
