21094bce3514d4895fa6efd2472d93bd6b6a033f
Davi Petris <davi.petris@oracle.com>
2023-11-08 12:33:16 -0300

    approve handler working

--- libs/approval/handlers.go
+++ libs/approval/handlers.go
@@ -20,19 +20,17 @@ func ButtonHandlers() map[string]func(s *discordgo.Session, i *discordgo.Interac
 
 func ModalHandlers() map[string]func(s *discordgo.Session, i *discordgo.InteractionCreate) {
 	return map[string]func(s *discordgo.Session, i *discordgo.InteractionCreate){
-		"Reject": rejectModalHandler,
+		"Reject":  rejectModalHandler,
+		"Approve": approveModalHandler,
 	}
 }
 
-func approveItemHandler(s *discordgo.Session, i *discordgo.InteractionCreate) {
-	data := i.MessageComponentData()
-	if !(strings.HasPrefix(data.CustomID, "group_buy_item_") && strings.HasSuffix(data.CustomID, "_approve_btn")) {
+func approveModalHandler(s *discordgo.Session, i *discordgo.InteractionCreate) {
+	data := i.ModalSubmitData()
+	if !strings.HasPrefix(data.CustomID, "approved_") {
 		return
 	}
-	itemID := getItemIDfromEmbeds(i.Message.Embeds)
-	if itemID == "" {
-		log.Fatal("There's no item in this message")
-	}
+	itemID := strings.TrimPrefix(data.CustomID, "approved_")
 
 	client := mongorm.ConnectedClient()
 	defer mongorm.DisconnectClient(client)
@@ -57,6 +55,56 @@ func approveItemHandler(s *discordgo.Session, i *discordgo.InteractionCreate) {
 		log.Fatalf("Failed to create approved item: %v", err)
 	}
 
+	submit_message := "Item approved!"
+	embed := discordgo.MessageEmbed{
+		Fields: *model.Item.ParseToEmbedFields(),
+	}
+	err = s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+		Type: discordgo.InteractionResponseUpdateMessage,
+		Data: &discordgo.InteractionResponseData{
+			Embeds:  []*discordgo.MessageEmbed{&embed},
+			Content: submit_message,
+		},
+	})
+	if err != nil {
+		log.Panicf("Unable to respond to modal %v: %v", data.CustomID, err)
+	}
+}
+
+func approveItemHandler(s *discordgo.Session, i *discordgo.InteractionCreate) {
+	data := i.MessageComponentData()
+	if !(strings.HasPrefix(data.CustomID, "group_buy_item_") && strings.HasSuffix(data.CustomID, "_approve_btn")) {
+		return
+	}
+	itemID := getItemIDfromEmbeds(i.Message.Embeds)
+	if itemID == "" {
+		log.Fatal("There's no item in this message")
+	}
+
+	responseData := discordgo.InteractionResponseData{
+		CustomID: "approved_" + itemID,
+		Title:    "Do you really want to approve this crap?",
+		Components: []discordgo.MessageComponent{
+			discordgo.ActionsRow{
+				Components: []discordgo.MessageComponent{
+					discordgo.TextInput{
+						CustomID:    "useless",
+						Style:       discordgo.TextInputParagraph,
+						Placeholder: "Useless field coz I can't create modals without components. So yeah, blame Discord",
+					},
+				},
+			},
+		},
+	}
+	response := discordgo.InteractionResponse{
+		Type: discordgo.InteractionResponseModal,
+		Data: &responseData,
+	}
+
+	err := s.InteractionRespond(i.Interaction, &response)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
 func rejectModalHandler(s *discordgo.Session, i *discordgo.InteractionCreate) {
