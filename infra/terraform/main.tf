d8a5d5ee33ce7d7abc19cedb9f3d42509ef7c94b
Davi Petris <davi.petris@oracle.com>
2023-11-10 12:27:57 -0300

    fix terraform

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -1,13 +1,3 @@
-# terraform {
-#   backend "remote" {
-#     organization = "DaviPtrs"
-#     workspaces {
-#       name = "group-buy-bot"
-#     }
-#   }
-# }
-
-
 terraform {
   cloud {
     organization = "DaviPtrs"
@@ -17,10 +7,10 @@ terraform {
   }
 }
 provider "oci" {
-  tenancy_ocid     = var.tenancy_ocid
-  user_ocid        = var.user_ocid
-  fingerprint      = var.fingerprint
-  private_key_path = var.private_key_path
-  region           = var.region
+  tenancy_ocid = var.tenancy_ocid
+  user_ocid    = var.user_ocid
+  fingerprint  = var.fingerprint
+  private_key  = var.private_key
+  region       = var.region
 }
 
--- infra/terraform/variables.tf
+++ infra/terraform/variables.tf
@@ -1,6 +1,6 @@
 variable "user_ocid" {}
 variable "fingerprint" {}
-variable "private_key_path" {}
+variable "private_key" {}
 variable "tenancy_ocid" {}
 
 variable "compartment_ocid" {
