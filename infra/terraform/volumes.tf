2c0b95972605ac224c226875f76cf252f42d3c6c
Davi Petris <davi.petris@oracle.com>
2023-11-11 05:59:50 -0300

    create_before_destroy for volume

--- infra/terraform/volumes.tf
+++ infra/terraform/volumes.tf
@@ -4,6 +4,10 @@ resource "oci_core_volume" "instance-datadisk" {
   display_name        = "group-buy-bot_datadisk"
   size_in_gbs         = var.datadisk_size_in_gbs
   vpus_per_gb         = "10"
+
+  lifecycle {
+    create_before_destroy = false
+  }
 }
 
 resource "oci_core_volume_attachment" "instance-datadisk" {
